cfc3d75eada8ffb40e73ccf05c70fa8f4e10c35a
Dejorden <d.moerman@hotmail.nl>
2023-09-30 13:07:09 +0200

    change px to rem

--- js/main.js
+++ js/main.js
@@ -8,7 +8,9 @@ const menuList = document.getElementById("mobile-menu-list-js");
 mobileMenu.onclick = () => {
     if (menuList.classList.contains('open')) {
         menuList.classList.remove('open');
+        mobileMenu.classList.remove('cross');
     } else {
         menuList.classList.add('open');
+        mobileMenu.classList.add('cross');
     }
 }
